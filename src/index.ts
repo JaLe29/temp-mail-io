f616e135c37af8a8c08ade17282370369543a4ca
JaLe <jaledeveloper@gmail.com>
2024-07-21 20:58:12 +0200

    Add fetchEmailsWithWait and prety code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 interface NewResponse {
 	email: string;
-	token: string
+	token: string;
 }
 
 interface FetchEmailsPureResponse {
@@ -14,7 +14,7 @@ interface FetchEmailsPureResponse {
 	body_text: string;
 	body_html: string;
 	created_at: string;
-	attachments: {id:string, name:string, size:number}[];
+	attachments: { id: string; name: string; size: number }[];
 }
 
 interface FetchEmailsResponse {
@@ -26,10 +26,10 @@ interface FetchEmailsResponse {
 	bodyText: string;
 	bodyHtml: string;
 	createdAt: Date;
-	attachments: {id:string, name:string, size:number, url: string}[];
+	attachments: { id: string; name: string; size: number; url: string }[];
 }
 
-const BASE_API = "https://api.internal.temp-mail.io";
+const BASE_API = 'https://api.internal.temp-mail.io';
 
 /**
  * Create a new email address using the temp-mail.io service
@@ -37,13 +37,13 @@ const BASE_API = "https://api.internal.temp-mail.io";
  * @returns email and token
  */
 export const newEmail = async () => {
-	const url = `${BASE_API}/api/v3/email/new`
+	const url = `${BASE_API}/api/v3/email/new`;
 
 	const data = await axios.post(url);
-	const response = data.data as NewResponse
+	const response = data.data as NewResponse;
 
-	return response
-}
+	return response;
+};
 
 /**
  * Fetch emails from the temp-mail.io service using the email address
@@ -52,26 +52,45 @@ export const newEmail = async () => {
  * @returns list of emails
  */
 export const fetchEmails = async (email: string) => {
-	const url = `${BASE_API}/api/v3/email/${email}/messages`
+	const url = `${BASE_API}/api/v3/email/${email}/messages`;
 	const data = await axios.get(url);
-	const response = data.data as FetchEmailsPureResponse[]
-	const transformedResponse = response.map((email) => {
-		return {
-			...email,
-			bodyText: email.body_text,
-			bodyHtml: email.body_html,
-			createdAt: new Date(email.created_at),
-			attachments: email.attachments.map((attachment) => {
-				return {
+	const response = data.data as FetchEmailsPureResponse[];
+	const transformedResponse = response.map(
+		email =>
+			({
+				...email,
+				bodyText: email.body_text,
+				bodyHtml: email.body_html,
+				createdAt: new Date(email.created_at),
+				attachments: email.attachments.map(attachment => ({
 					...attachment,
-					url: `${BASE_API}/api/v3/attachment/${attachment.id}?download=1`
-				}
-			})
-		} as FetchEmailsResponse
-	})
+					url: `${BASE_API}/api/v3/attachment/${attachment.id}?download=1`,
+				})),
+			}) as FetchEmailsResponse,
+	);
 
-	return transformedResponse as FetchEmailsResponse[]
-}
+	return transformedResponse as FetchEmailsResponse[];
+};
+
+/**
+ * Fetch emails from the temp-mail.io service using the email address with a timeout
+ *
+ * @param email email address
+ * @param expectedEmails expected number of emails
+ * @param timeout timeout in milliseconds
+ * @returns list of emails
+ */
+export const fetchEmailsWithWait = async (email: string, expectedEmails: number, timeout: number = 30000) => {
+	let emails: FetchEmailsResponse[] = [];
+	let currentTimeout = 0;
+	while (emails.length < expectedEmails && currentTimeout < timeout) {
+		emails = await fetchEmails(email);
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		currentTimeout += 1000;
+	}
+
+	return emails;
+};
 
 /**
  * Delete an email from the temp-mail.io service
@@ -80,6 +99,6 @@ export const fetchEmails = async (email: string) => {
  * @param token from newEmail response
  */
 export const deleteEmail = async (email: string, token: string) => {
-	const url = `${BASE_API}/api/v3/email/${email}`
-	await axios.delete(url, {data: {token}});
-}
+	const url = `${BASE_API}/api/v3/email/${email}`;
+	await axios.delete(url, { data: { token } });
+};
